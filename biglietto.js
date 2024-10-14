ce53c3efa8bd8607832f8e330636fccd306db1b8
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-14 13:53:40 +0200

    ADD check userAge/totalKm value

--- biglietto.js
+++ biglietto.js
@@ -1,9 +1,20 @@
 console.log("Esercizio Biglietto del treno")
 
 let userAge = parseInt(prompt("Inserisci la tua età: "))
+
+while(isNaN(userAge) || userAge === 0 )
+{
+    alert("Attenzione, non hai inserito un numero valido!")
+    userAge = parseInt(prompt("Inserisci la tua età: "))
+}
 //console.log(userAge)
 
 let totalKm = parseInt(prompt("Inserisci il numero di chilometri del tuo viaggio: "))
+while(isNaN(totalKm) || totalKm === 0 )
+    {
+        alert("Attenzione, non hai inserito un numero valido!")
+        totalKm = parseInt(prompt("Inserisci il numero di chilometri del tuo viaggio: "))
+    }
 //console.log(totalKm)
 
 let priceKm = 0.21

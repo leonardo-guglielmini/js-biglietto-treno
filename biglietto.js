a679c6f1fa1763cf99815a69ffd41336da29def8
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-14 13:45:10 +0200

    ADD exercise

--- biglietto.js
+++ biglietto.js
@@ -1,2 +1,24 @@
 console.log("Esercizio Biglietto del treno")
 
+let userAge = parseInt(prompt("Inserisci la tua età: "))
+//console.log(userAge)
+
+let totalKm = parseInt(prompt("Inserisci il numero di chilometri del tuo viaggio: "))
+//console.log(totalKm)
+
+let priceKm = 0.21
+let basePrice = totalKm * priceKm
+let discount = 0
+//console.log(basePrice)
+
+if (userAge < 18){
+    discount = (basePrice*20) /100
+} else if (userAge > 65){
+    discount = (basePrice*40) /100
+}
+
+let finalPrice = (basePrice-discount).toFixed(2)
+let message = `Il prezzo finale della corsa costa ${finalPrice} €`
+
+console.log(message)
+
